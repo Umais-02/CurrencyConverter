cc6f07c139b1cb1a4ccef100bea1c1e0b4475d18
Umais-02 <m.umays.252@gmail.com>
2026-09-10 22:29:05 +0500

    improved my code

--- HumanVersion.py
+++ HumanVersion.py
@@ -2,9 +2,9 @@ print("Currency Converter")
 pkr = float(input("Enter amount in PKR: "))
 
 print("Choose currency:")
-print("1. USD")
-print("2. Pounds")
-print("3. Turkish Lira")
+print("1:USD")
+print("2:Pounds")
+print("3:Turkish Lira")
 
 choice = int(input("Enter your choice: "))
 
@@ -15,7 +15,7 @@ elif choice == 2:
     pounds = pkr/374.20
     print("PKR", pkr, "=", pounds, "Pounds")
 elif choice == 3:
-    lira = pkr / 6.50
+    lira = pkr/6.50
     print("PKR", pkr, "=", lira, "Turkish Lira")
 else:
-    print("Invalid choice.")
+    print("Invalid choice")
